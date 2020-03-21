8cf311f63969837755e0ca9678f239c920f8e41a
Jon <jonhualde3@gmail.com>
2020-03-21 10:15:23 +0100

    changes on host

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-lively-42930',
     user : 'postgres',
     password : '$Ragnarok1994$',
     database : 'smart-brain'
